e2c81f8d4773cd4f83515ab24022a9a9213f65da
JM <jayeshmaheshwari555@gmail.com>
2025-06-01 17:53:29 +0530

    Add solutions for frequency of the most frequent element and triangle type determination

---
+++ leetcode/3024. Type of Triangle.py
@@ -0,0 +1,12 @@
+class Solution:
+    def triangleType(self, nums: List[int]) -> str:
+        if not (nums[0]+nums[1] > nums[2] and nums[0]+nums[2] > nums[1] and nums[1]+nums[2] > nums[0]):
+            return "none"
+
+        if len(set(nums)) == 1:
+            return "equilateral"
+
+        if len(set(nums)) == 2:
+            return "isosceles"
+
+        return "scalene"
